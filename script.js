8a1a53ea3968a9924407b3cf50a36c3d9d28af3c
Amar Emini <128543379+amarua05@users.noreply.github.com>
2024-03-06 01:30:42 +0100

    hello world

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("submitBtn").onclick = function() {
+  console.log("Button clicked");
+  const birthdayValue = document.getElementById("birthday").value;
+  console.log(birthdayValue);
+  const [year, month, date] = birthdayValue.split("-");
+  
+  fetch("/data.json")
+    .then(response => response.json())
+    .then(data => {
+      const matchingData = data.find(item => item.date === `${month}-${date}`);
+      if (matchingData) {
+        const rank = matchingData.rank;
+        const rarity = matchingData.rarity;
+        const rarityCalc = Math.round((rank / 366) * 10);
+        
+        document.getElementById("rarity").value = rarityCalc;
+
+        document.getElementById("slider").value = rarityCalc;
+
+        document.getElementById("rank").value = rank;
+
+        document.getElementById("births").value = rarity;
+
+        document.getElementById("errorMessage").innerText = "";
+      } else {
+        console.log("No matching data found");
+        document.getElementById("errorMessage").innerText = "Wrong Input! Please enter a valid date.";
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    });
+};
